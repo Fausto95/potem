957137a4fddd05a0caf15911c0980b585079cce2
Faustino Kialungila <Faustino.kialungila@gmail.com>
2023-07-13 19:05:03 +0200

    Initial Commit

---
+++ Potem/PotemApp.swift
@@ -0,0 +1,17 @@
+//
+//  PotemApp.swift
+//  Potem
+//
+//  Created by Faustino KIALUNGILA on 13/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct PotemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
