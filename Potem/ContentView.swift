6345eb3a83833f38cf7b1e5ce4a06b2a1c97fe7c
Faustino Kialungila <Faustino.kialungila@gmail.com>
2023-07-22 14:59:25 +0200

    feat: listing apps

--- Potem/ContentView.swift
+++ Potem/ContentView.swift
@@ -7,17 +7,63 @@
 
 import SwiftUI
 
-struct ContentView: View {
+
+
+struct ListViewItem: View {
+    var name: String;
+    var duration: String
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+            Text(name)
+            Text(duration)
         }
-        .padding()
     }
 }
+
+struct ListView: View {
+    var registers: [Registers]
+    
+    var body: some View {
+        VStack {
+            ForEach(DATA_FIXTURES) { fixture in
+                ListViewItem(name: fixture.appName, duration: fixture.duration)
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+
+    var body: some View {
+        VStack {
+            HStack {
+                Text("App")
+                Text("Duration")
+                
+            }
+            VStack {
+                ListView(registers: DATA_FIXTURES)
+            }
+            .padding()
+        }
+    }
+}
+
+struct Registers: Identifiable {
+    var id = UUID()
+    var appName: String;
+    var duration: String;
+}
+
+let DATA_FIXTURES = [
+    Registers(appName: "VS Code", duration: "33min"),
+    Registers(appName: "Zed Preview", duration: "40min"),
+    Registers(appName: "Google Chrome", duration: "40min"),
+    Registers(appName: "Xcode", duration: "40min"),
+    Registers(appName: "Notion", duration: "40min"),
+    Registers(appName: "Arc", duration: "40min")
+]
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
